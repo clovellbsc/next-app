46e08c5622f4c8c60d96f3b5abc560ddcef9a4f6
Chris Lovell <clovellbsc@gmail.com>
2022-08-16 13:00:36 +0100

    basic next app set up

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Link href="/posts/first-post">First Post</Link>
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/posts/first-post/index.tsx
@@ -0,0 +1,9 @@
+const FirstPost = () => {
+  return (
+    <div>
+      <p>Hi I am a post!</p>
+    </div>
+  );
+};
+
+export default FirstPost;
