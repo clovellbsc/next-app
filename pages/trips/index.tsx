885432cefeb725e7b10d04bb237ee23831bc19fa
Chris Lovell <clovellbsc@gmail.com>
2022-08-24 17:02:33 +0100

    Basic structure for trips page

---
+++ pages/trips/index.tsx
@@ -0,0 +1,14 @@
+const Trips = () => {
+  return (
+    <div>
+      <div>Trips</div>
+      <div>
+        <p>Barcelona</p>
+        <p>Mexico</p>
+        <p>Australia</p>
+      </div>
+    </div>
+  );
+};
+
+export default Trips;
