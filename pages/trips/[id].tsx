69ff49c4b3533acabbacde26d04528a3cea0a6e6
Chris Lovell <clovellbsc@gmail.com>
2022-08-25 16:58:10 +0100

    added trip component

---
+++ pages/trips/[id].tsx
@@ -0,0 +1,16 @@
+const Trip = () => {
+  return (
+    <div>
+      <div>
+        <p>Barcelona</p>
+      </div>
+      <div>
+        <p>flights</p>
+        <p>visa</p>
+        <p>accommodation</p>
+      </div>
+    </div>
+  );
+};
+
+export default Trip;
